119a96acca702d60bc3eda0bd42fc4569dcaccf3
Olav Sundfør <olav@sundfoer.com>
2020-01-30 11:26:24 +0100

    only creating app if not docker.

--- index.js
+++ index.js
@@ -21,54 +21,66 @@ heroku.branch = core.getInput("branch");
 heroku.dontuseforce = core.getInput("dontuseforce");
 heroku.useDocker = core.getInput("useDocker"); 
 
+function deploy(useForce) {
+
+    const force = useForce? " --force": ""; 
+
+    if (useForce) {
+
+        execSync("heroku container:push web" + force); 
+        execSynd("heroku container:release web" + force);
+    } else {
+
+        execSync(`git push heroku ${heroku.branch}:master` + force); 
+    }
+}
+
+function addRemote() {
+    try {
+
+        execSync("heroku git:remote --app " + heroku.app_name);
+        console.log("Added git remote heroku");
+      } catch (err) {
+
+          execSync(
+          "heroku create " +
+            heroku.app_name +
+            (heroku.buildpack ? " --buildpack " + heroku.buildpack : "")
+        );
+        console.log("Successfully created a new heroku app");
+      }
+}
+
 try {
-  //execSync("git fetch --prune --unshallow");
-  execSync(createCatFile(heroku));
-  console.log("Created and wrote to ~./netrc");
-  execSync("heroku login");
-  console.log("Successfully logged into heroku");
+   //execSync("git fetch --prune --unshallow");
+    execSync(createCatFile(heroku));
+    console.log("Created and wrote to ~./netrc");
+    execSync("heroku login");
+    console.log("Successfully logged into heroku");
 
-  try {
-    execSync("heroku git:remote --app " + heroku.app_name);
-    console.log("Added git remote heroku");
-  } catch (err) {
-    execSync(
-      "heroku create " +
-        heroku.app_name +
-        (heroku.buildpack ? " --buildpack " + heroku.buildpack : "")
-    );
-    console.log("Successfully created a new heroku app");
-  }
-    
-  function deploy(useForce) {
+    if (!heroku.useDocker) {
         
-        const force = useForce? " --force": ""; 
-      
-        if (useDocker) {
-            
-            execSync("heroku container:push web" + force); 
-            execSynd("heroku container:release web" + force);
-        } else {
-            
-            execSync(`git push heroku ${heroku.branch}:master` + force); 
-        }
-  }
+        addRemote(); 
+    }
+   
 
-  try {
+    try {
     
-      deploy(false);   
-  } catch (err) {
+        deploy(false);   
+    } catch (err) {
     
-      LLconsole.log(
-        "Unable to push branch because the branch is behind the deployed branch. Using --force to deploy branch. (If you want to avoid this, set dontuseforce to 1 in with: of .github/workflows/action.yml"
-      );
+        LLconsole.log(
+            "Unable to push branch because the branch is behind the deployed branch. Using --force to deploy branch. (If you want to avoid this, set dontuseforce to 1 in with: of .github/workflows/action.yml"
+        );
       
-    deploy(!heroku.dontUseForce);
-  }
-  core.setOutput(
-    "status",
-    "Successfully deployed heroku app from branch " + heroku.branch
-  );
+        deploy(!heroku.dontUseForce);
+    }
+      
+    core.setOutput(
+        "status",
+        "Successfully deployed heroku app from branch " + heroku.branch
+    );
 } catch (err) {
-  core.setFailed(err.toString());
+    
+    core.setFailed(err.toString());
 }
