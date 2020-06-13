8a762b8edde84d9a4f14f5464cb169f78c9b8ee4
AkhileshNS <nsakhilesh02@gmail.com>
2020-06-13 22:45:36 +0530

    Removed conditional checking of html in test-action

--- scripts/test-action.js
+++ scripts/test-action.js
@@ -12,13 +12,7 @@ https
 
     // The whole response has been received. Print out the result.
     resp.on("end", () => {
-      console.log(rawData);
-      let data;
-      if (!rawData.includes("<body>")) {
-        data = JSON.parse(rawData);
-      } else {
-        data = JSON.parse(rawData.split("<body>")[1].split("</body>")[0]);
-      }
+      const data = JSON.parse(rawData);
 
       if (
         data.GITHUB_RUN_ID === process.env.GITHUB_RUN_ID &&
@@ -27,6 +21,8 @@ https
         data.GITHUB_REF === process.env.GITHUB_REF
       ) {
         core.setOutput("status", "Test Success");
+      } else {
+        core.setFailed("Test Failed: Please check logs to see source of error");
       }
     });
   })
