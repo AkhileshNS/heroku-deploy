9df72afa0eda226c3358b4ea589621cfc97ef7f7
AkhileshNS <nsakhilesh02@gmail.com>
2021-05-29 22:14:18 +0530

    Updated configureGit

--- src/steps/configureGit.ts
+++ src/steps/configureGit.ts
@@ -3,30 +3,32 @@ import { execSync } from 'child_process';
 import { IHeroku } from '../types';
 import { ansi_colors } from '../util';
 
-export const configureGit = (heroku: IHeroku): boolean => {
-  core.debug(ansi_colors.cyan + "STEP: Configuring git");
-
+const configureUserAndCommit = (heroku: IHeroku): boolean => {
   execSync(`git config user.name "Heroku-Deploy"`);
   execSync(`git config user.email "${heroku.email}"`);
-  const status = execSync("git status --porcelain").toString().trim();
-  if (status) {
+  if (execSync("git status --porcelain").toString().trim()) {
     execSync(
       'git add -A && git commit -m "Commited changes from previous actions"'
     );
   }
+  return true;
+}
 
-  // Check if using Docker and unshallow repo if not
-  if (!heroku.usedocker) {
-    // Check if Repo clone is shallow
-    const isShallow = execSync(
-      "git rev-parse --is-shallow-repository"
-    ).toString().trim();
-
-    // If the Repo clone is shallow, make it unshallow
-    if (isShallow === "true") {
-      execSync("git fetch --prune --unshallow");
-    }
+const unshallowRepo = (heroku: IHeroku): boolean => {
+  if (heroku.usedocker) {
+    return true;
   }
+  if (execSync("git rev-parse --is-shallow-repository").toString().trim()==="true") {
+    execSync("git fetch --prune --unshallow");
+  }
+  return true;
+}
+
+export const configureGit = (heroku: IHeroku): boolean => {
+  core.debug(ansi_colors.cyan + "STEP: Configuring git");
+
+  configureUserAndCommit(heroku) &&
+  unshallowRepo(heroku)
 
   core.info(ansi_colors.green + "STEP: Configuring git - Success")
   return true;
