163d970f3709c47e412ffe0c4c493b9c6c01b435
AkhileshNS <nsakhilesh02@gmail.com>
2021-10-27 13:12:00 -0400

    Running heroku version in action

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello world");
+import {exec} from 'child_process';
+import {promisify} from 'util';
+
+const asyncExec = promisify(exec);
+
+(async () => {
+  try {
+    console.log((await asyncExec("heroku --version")).stdout);
+  } catch (err) {
+    console.log(err);
+  }
+})()
